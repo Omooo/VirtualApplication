6c04f65f6f67f26f8762c85d4e7bf6ce905d2f2e
Omooo <869759698@qq.com>
2020-04-29 12:01:49 +0800

    Initial commit

---
+++ plugin_package/src/main/java/top/omooo/plugin_package/PluginActivity.java
@@ -0,0 +1,14 @@
+package top.omooo.plugin_package;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PluginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
